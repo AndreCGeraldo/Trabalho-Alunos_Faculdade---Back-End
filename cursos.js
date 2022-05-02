5c0b3e5f4c2aefae0223953a603b990dc8acd508
André da Cruz Geraldo <andrecruz2007@hotmail.com>
2022-05-02 00:46:28 -0300

    :arrow_up: Rotas atualizadas com Comentarios

--- cursos.js
+++ cursos.js
@@ -5,65 +5,65 @@ const dbKnex = require("./data/db_config");
 
 router.use(express.json());
 
-
+// Mostra a listagem dos Cursos
 router.get("/", async (req, res) => {
-    try {
-        const cursos = await dbKnex("cursos").orderBy("id", "desc");
-        res.status(200).json(cursos);
-    } catch (error) {
-        res.status(400).json({ msg: error.message });
-    }
+  try {
+    const cursos = await dbKnex("cursos").orderBy("id", "desc");
+    res.status(200).json(cursos);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
 });
 
 
-
+// Faz a inclusão dos Cursos no Banco
 router.post("/", async (req, res) => {
-    const { instituicao, nomeCurso, fone } = req.body;
+  const { instituicao, nomeCurso, fone } = req.body;
 
-    if (!instituicao || !nomeCurso || !fone) {
-        res.status(400).json({ msg: "Enviar instituição, nomeCurso, e fone do curso" });
-        return;
-    }
-    try {
-        const novo = await dbKnex("cursos").insert({ instituicao, nomeCurso, fone });
-        res.status(201).json({ id: novo[0] });
-    } catch (error) {
-        res.status(400).json({ msg: error.message });
-    }
+  if (!instituicao || !nomeCurso || !fone) {
+    res.status(400).json({ msg: "Enviar instituição, nomeCurso, e fone do curso" });
+    return;
+  }
+  try {
+    const novo = await dbKnex("cursos").insert({ instituicao, nomeCurso, fone });
+    res.status(201).json({ id: novo[0] });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
 });
 
-
+// Faz alteração nos Cursos no banco
 router.put("/:id", async (req, res) => {
-    const id = req.params.id;
-    const { instituicao, nomeCurso, fone } = req.body;
-    try {
-      await dbKnex("cursos").update({ instituicao, nomeCurso, fone }).where("id", id);
-      res.status(200).json();
-    } catch (error) {
-      res.status(400).json({ msg: error.message });
-    }
-  });
-  
-  
-  router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-      await dbKnex("cursos").del().where({ id });
-      res.status(200).json();
-    } catch (error) {
-      res.status(400).json({ msg: error.message });
-    }
-  });
+  const id = req.params.id;
+  const { instituicao, nomeCurso, fone } = req.body;
+  try {
+    await dbKnex("cursos").update({ instituicao, nomeCurso, fone }).where("id", id);
+    res.status(200).json();
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+});
 
+// Faz a exclusão dos Cursos no banco
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await dbKnex("cursos").del().where({ id });
+    res.status(200).json();
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+});
 
-  router.get("/resumo", async (req, res) => {
-    try {
-      const numDeCursos = await dbKnex("cursos").select("instituicao").count({ numero_de_cursos: "id" }).groupBy("instituicao");
-      res.status(200).json(numDeCursos);
-    } catch (error) {
-      res.status(400).json({ msg: error.message });
-    }
-  });
+// Mostra o resumo das Instituições e cursos
+router.get("/resumo", async (req, res) => {
+  try {
+    const numDeCursos = await dbKnex("cursos").select("instituicao").count({ numero_de_cursos: "id" }).groupBy("instituicao");
+    res.status(200).json(numDeCursos);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+});
 
 
 
